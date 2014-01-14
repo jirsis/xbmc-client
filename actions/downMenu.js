b78d53f834e099036d584710772fd6b2fc36dcc6
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-01-14 21:33:49 +0100

    Implementation of message to go down in menu

--- actions/downMenu.js
+++ actions/downMenu.js
@@ -1,11 +1,7 @@
-var downMenu = {
+var down = {
   jsonrpc: "2.0",
   method: "Input.Down",
   id: 1
 };
 
-var action = function(host){
-  return "http://" + host + JSON.stringify(contextMenu);
-}
-
-exports.downMenuAction = action;
+exports.action = down;
